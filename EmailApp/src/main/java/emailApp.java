e1c2cc2cb95d9bebdfed48c8062dcbf2646fdac2
Brandon-Terry <74943333+Brandon-Terry@users.noreply.github.com>
2021-05-27 09:48:21 -0400

    Add files via upload

---
+++ EmailApp/src/main/java/emailApp.java
@@ -0,0 +1,13 @@
+public class emailApp {
+
+    public static void main(String[] args) {
+        Email em1 = new Email("Braxton","Terry");
+        Email em2 = new Email("Harper","Terry");
+        System.out.println(em1.showInfo());
+
+
+
+
+    }
+
+}
